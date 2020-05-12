d01f5ee3c0f841af4d42b416a929eae32fedbbcd
Ckwei <kkww0259@gmail.com>
2020-05-12 21:39:06 +0800

    add

---
+++ midterm/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+// The number of labels (without negative)
+#define label_num 4
+
+struct Config {
+  // This must be the same as seq_length in the src/model_train/config.py
+  const int seq_length = 64;
+
+  // The number of expected consecutive inferences for each gesture type.
+  const int consecutiveInferenceThresholds[label_num] = {7, 1, 20, 5};
+  const char* output_message[label_num] = {
+        "downhit:\n\r",
+        "forwardhit:\n\r",
+        "waveleft:\n\r",
+        "waveright:\n\r"};
+};
+
+Config config;
+#endif // CONFIG_H_
